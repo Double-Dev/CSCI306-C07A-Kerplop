a925d96ccc41f3f35c9dcffe60401c23dd680dd5
double-dev <deven.layton@gmail.com>
2025-09-22 14:55:25 -0600

    Added JackOLantern and Zombie pieces.

---
+++ src/levelPieces/JackOLantern.java
@@ -0,0 +1,20 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.InteractionResult;
+
+public class JackOLantern extends GamePiece {
+
+	public JackOLantern(int location) {
+		super('@', "Jack O' Lantern", location);
+	}
+
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		if (this.getLocation() == playerLocation) {
+			return InteractionResult.GET_POINT;
+		}
+		return InteractionResult.NONE;
+	}
+
+}
---
+++ src/levelPieces/Zombie.java
@@ -0,0 +1,35 @@
+package levelPieces;
+
+import gameEngine.Drawable;
+import gameEngine.GameEngine;
+import gameEngine.InteractionResult;
+import gameEngine.Moveable;
+
+public class Zombie extends GamePiece implements Moveable {
+
+	public Zombie(int location) {
+		super('Z', "Zombie", location);
+	}
+
+	@Override
+	public void move(Drawable[] gameBoard, int playerLocation) {
+		if (playerLocation < this.getLocation()
+				&& this.getLocation() > 0
+				&& gameBoard[this.getLocation()-1] == null) {
+			this.setLocation(this.getLocation()-1);
+		} else if (playerLocation > this.getLocation()
+				&& this.getLocation() < GameEngine.BOARD_SIZE-1
+				&& gameBoard[this.getLocation()+1] == null) {
+			this.setLocation(this.getLocation()+1);
+		}
+	}
+
+	@Override
+	public InteractionResult interact(Drawable[] gameBoard, int playerLocation) {
+		if (this.getLocation() == playerLocation) {
+			return InteractionResult.HIT;
+		}
+		return InteractionResult.NONE;
+	}
+	
+}
