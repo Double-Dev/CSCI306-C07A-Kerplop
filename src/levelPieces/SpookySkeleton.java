5a290763aec9abc3110318c3c78473dd437b521e
Kazahana45 <josephchamberlain46@gmail.com>
2025-09-23 21:33:11 -0600

    JC - Finished implementing Spooky Scary Skeleton

--- src/levelPieces/SpookySkeleton.java
+++ src/levelPieces/SpookySkeleton.java
@@ -8,16 +8,24 @@ import gameEngine.Moveable;
 // 1 of our 2 moveable game pieces that interacts with the Player
 
 public class SpookySkeleton extends GamePiece implements Moveable {
+	private java.util.Random rand = new java.util.Random();
+	
 	public SpookySkeleton(char symbol, String label, int location) {
 		super('W', "Spooky Scary Skeleton!", location);
 	}
 	
 	
 	public void move(Drawable[] gameBoard, int playerLocation) { // A wildcard that can move anywhere from 2 to the left or 2 to the right
-		int[] moving = {-1, -2, 0, 1, 2};
-		java.util.Random rand = new java.util.Random();
-		int leap = rand.nextInt(5) - 1;
+		int[] moving = {-2, -1, 0, 1, 2};
+		int randnum = rand.nextInt(5) - 1;
+		int leap = moving[randnum]; // Chooses a random number to move from -2 to 2
 		
+		if (gameBoard[this.getLocation() + leap] == null) { // As long as the spot is available, it will move there
+			this.setLocation(this.getLocation());
+		} 
+		else {
+			this.setLocation(this.getLocation() + leap);
+		}
 	}
 	
 	public InteractionResult interact(Drawable [] gameBoard, int playerLocation) {
